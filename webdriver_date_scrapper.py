7a6942b078a3a7f4a4cfee5be04ea395e6ee7f49
Jarry Ngandjui <jarryngandjui@gmail.com>
2023-08-20 13:05:54 -0400

    Scrape dates with selenium and bs4

--- webdriver_date_scrapper.py
+++ webdriver_date_scrapper.py
@@ -4,23 +4,35 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.common.by import By 
 from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
 
-
-# start by defining the options 
-options = webdriver.ChromeOptions() 
-options.add_argument('--headless') 
-options.page_load_strategy = 'none' 
-
-chrome_service = Service(ChromeDriverManager().install() ) 
-driver = Chrome(options=options, service=chrome_service) 
-driver.implicitly_wait(5)
-
-url = "https://www.usab.com/teams/5x5-mens-world-cup/schedules" 
-driver.get(url) 
-time.sleep(10)
+# Create Chrome options with headless mode
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')  # Enable headless mode
+driver = webdriver.Chrome(options=chrome_options)
 
 try:
-    content = driver.find_elements(By.CSS_SELECTOR, "time[class*='uppercase']")
-    print(content.text)
+    url = "https://www.usab.com/teams/5x5-mens-world-cup/schedules" 
+    driver.get(url) 
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # Find the element containing the game schedule
+    schedule_list = soup.find_all('div', class_='font-title')
+    # print("Schedule list", schedule_list)
+
+    if schedule_list:
+        # Extract information from the table rows
+        for date_entry in schedule_list:
+            # print(date_entry)
+            month_time = date_entry.find('time', class_='uppercase').get_text()
+            day_text = date_entry.find('time', class_='days').get_text()
+            
+            # Events in the past have no time element
+            time_element = date_entry.find('span', class_='uppercase')
+            time_text = time_element.get_text() if time_element else ""
+            
+            print(f"Month: {month_time}\nDay: {day_text}\nTime: {time_text}\n")
+    else:
+        print("No schedule table found on the page.")
 finally:
     driver.quit()
