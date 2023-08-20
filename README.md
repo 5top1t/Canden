913dbd89a957a838cfee3ebaf99a9ad031037cdf
Jarry Ngandjui <jarryngandjui@gmail.com>
2023-08-20 05:30:49 -0400

    Read calendars

---
+++ README.md
@@ -0,0 +1,3 @@
+# Canden
+
+Ever wanted to create a calendar or event for a concert, sport team, artist tour, trash collections give Canden a try. Canden creates calendar events from a webpage so you never miss what is important.
---
+++ google_calendar_client.py
@@ -0,0 +1,36 @@
+import sys
+
+from google.auth.exceptions import RefreshError
+from googleapiclient import sample_tools
+
+
+def main(argv):
+    # Authenticate and construct service.
+    service, flags = sample_tools.init(
+        argv,
+        "calendar",
+        "v3",
+        __doc__,
+        __file__,
+        scope="https://www.googleapis.com/auth/calendar",
+    )
+
+    try:
+        page_token = None
+        while True:
+            calendar_list = service.calendarList().list(pageToken=page_token).execute()
+            for calendar_list_entry in calendar_list["items"]:
+                print(calendar_list_entry["summary"], calendar_list_entry["id"])
+            page_token = calendar_list.get("nextPageToken")
+            if not page_token:
+                break
+
+    except RefreshError:
+        print(
+            "The credentials have been revoked or expired, please re-run"
+            "the application to re-authorize."
+        )
+
+
+if __name__ == "__main__":
+    main(sys.argv)
