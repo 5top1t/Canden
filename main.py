3b8f384ec02fec1ffa4a0b6594c5e6bbf449693b
Jarry Ngandjui <jarryngandjui@gmail.com>
2023-08-21 10:38:21 -0400

    Create a main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from google_calendar_client import GoogleCalendarClient
+from webdriver_date_scrapper import get_event_dates
+
+timezone = "US/Eastern"
+
+calendar_client = GoogleCalendarClient()
+calendar_client.create_calendar("USA Basketball")
+print(calendar_client.calendars)
+
+event_dates = get_event_dates()
+for event_date in event_dates:
+    calendar_client.create_event("primary", "USA Basketball", event_date, event_date)
+# print(event_dates)
