6f87da7af4b73e858a1c6e1a1798de2dc86ee516
Jarry Ngandjui <jarryngandjui@gmail.com>
2023-08-21 11:09:48 -0400

    Create and add events to the calendar iff DNE

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from google_calendar_client import GoogleCalendarClient
 from webdriver_date_scrapper import get_schedule
 
 timezone = "US/Eastern"
+calendar_name = "USA Basketball"
+calendar_id = ""
+calendar_client = GoogleCalendarClient(timezone)
 
-# calendar_client = GoogleCalendarClient(timezone)
-# team_usa_calendar_id = calendar_client.create_calendar("USA Basketball")
-# print(calendar_client.calendars)
-
-schedule = get_schedule(timezone)
-for event in schedule:
-    # calendar_client.create_event(team_usa_calendar_id, "USA Basketball", event_date, event_date)
-    print(event)
+if calendar_name in calendar_client.calendars:
+    calendar_id = calendar_client.calendars[calendar_name]
+    print(f"{calendar_name} calendar already exists.")
+else:
+    calendar_id = calendar_client.create_calendar("USA Basketball")
+    print(f"Created {calendar_name} calendar.")
+    
+    schedule = get_schedule(timezone)
+    for event in schedule:
+        calendar_client.create_event(calendar_id, "USA Basketball", event.date, event.date)
